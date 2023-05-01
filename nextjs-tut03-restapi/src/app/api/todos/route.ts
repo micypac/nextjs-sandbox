23c51bd52a45557728810920ecf0cbf02aa8e17b
Michael Pacleb <michael.pacleb@gmail.com>
2023-05-01 10:37:12 -0400

    implemented POST todos route.

--- nextjs-tut03-restapi/src/app/api/todos/route.ts
+++ nextjs-tut03-restapi/src/app/api/todos/route.ts
@@ -1,14 +1,40 @@
 import { NextResponse } from "next/server";
 
-const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
-
+const SOURCE_URL = process.env.DATA_SOURCE_URL as string;
 const API_KEY = process.env.DATA_API_KEY as string;
 
 export async function GET() {
-  const res = await fetch(DATA_SOURCE_URL);
+  const res = await fetch(SOURCE_URL);
   console.log(API_KEY);
 
   const todos: Todo[] = await res.json();
 
   return NextResponse.json(todos);
+}
+
+export async function POST(request: Request) {
+  const { userId, title }: Partial<Todo> = await request.json();
+
+  if (!userId || !title) {
+    return NextResponse.json({
+      message: "Missing required data",
+    });
+  }
+
+  const res = await fetch(SOURCE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+    body: JSON.stringify({
+      userId,
+      title,
+      completed: false,
+    }),
+  });
+
+  const newTodo: Todo = await res.json();
+
+  return NextResponse.json(newTodo);
 }
