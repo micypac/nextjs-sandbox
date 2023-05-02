cb636d2aa4a55ad6b608f8fb30f2cd38149cd704
Michael Pacleb <michael.pacleb@gmail.com>
2023-05-01 23:16:10 -0400

    fixed dynamic GET route to extract id from second parameters as props.

--- nextjs-tut03-restapi/src/app/api/todos/[id]/route.ts
+++ nextjs-tut03-restapi/src/app/api/todos/[id]/route.ts
@@ -2,9 +2,15 @@ import { NextResponse } from "next/server";
 
 const SOURCE_URL = process.env.DATA_SOURCE_URL as string;
 
-export async function GET(request: Request) {
-  console.log(request.url);
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+// id is now coming from params and is the recommended Nextjs way
+export async function GET(request: Request, { params: { id } }: Props) {
+  // const id = request.url.slice(request.url.lastIndexOf("/") + 1);
 
   const res = await fetch(`${SOURCE_URL}/${id}`);
 
